e39ba0ada8464ab2bea3b61035928719f440e3d5
syobonpastel <syobon.pastel@gmail.com>
2021-05-07 21:58:09 +0900

    header

--- techfeelo_hp/src/components/header.js
+++ techfeelo_hp/src/components/header.js
@@ -9,32 +9,34 @@ const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `aliceblue`,
-      marginBottom: `1.45rem`,
+      padding: "0"
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        // margin: `0 auto`, /* 上下 | 左右 */
+        padding: `1.45rem 5% 0.5rem 2rem`, /* 上 | 右 | 下 | 左 */
       }}
     >
-      <h1 style={{ margin: 0}}>
-        <Link to="/">
-          <StaticImage
-            src="../images/techfeelo_logo.png"
-            width={300}
-            alt="techfeelo"
-          />
-          {/*{siteTitle}*/}
-        </Link>
-        <a className={Styles.menuBar} style={{ margin: 0, display: "inline-block"}}>
-          <Link className={Styles.button} to="/about/">法人について</Link>
-          <Link className={Styles.button} to="/info/">お知らせ</Link>
-          <Link className={Styles.button} to="/report/">活動報告</Link>
-          <Link className={Styles.button} to="/contact/">お問い合わせ</Link>
-        </a>
-      </h1>
+      <div>
+        <div style={{ width:"20%", display:"inline-block" }}>
+          <Link to="/">
+            <StaticImage
+              src="../images/techfeelo_logo.png"
+              // maxWidth={200}
+              alt="techfeelo"
+            />
+          </Link>
+        </div>
+        <nav className={Styles.nav}>
+          <ul className={Styles.menuBar}>
+            <li className={Styles.li}><Link className={Styles.button} to="/about/">法人について</Link></li>
+            <li className={Styles.li}><Link className={Styles.button} to="/info/">お知らせ</Link></li>
+            <li className={Styles.li}><Link className={Styles.button} to="/report/">活動報告</Link></li>
+            <li className={Styles.li}><Link className={Styles.button} to="/contact/">お問い合わせ</Link></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 )
