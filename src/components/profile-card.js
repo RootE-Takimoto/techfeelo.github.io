9bb48e16610e3803197e1982cee3b30f127f18ff
syobonpastel <syobon.pastel@gmail.com>
2021-05-16 00:49:23 +0900

    change officers page's style

--- src/components/profile-card.js
+++ src/components/profile-card.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+
 const useStyles = makeStyles({
     root: {
-        minWidth: 250,
-        maxWidth: 300,
+        width: 270,
+        // minWidth: 250,
+        // maxWidth: 300,
         margin: 15,
     },
     media: {
@@ -29,9 +34,24 @@ const useStyles = makeStyles({
     pos: {
         marginBottom: 12,
     },
-    twitter: {
-        backgroundColor: "#00acee",
-        color:"#ffffff",
+    comment: {
+        fontSize: "0.8rem",
+        width: 250,
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    button: {
+        marginBottom: 5,
+        marginLeft: 10,
+    },
+});
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#0066ff',
+            main: '#1DA1F2',
+        },
     },
 });
 
@@ -54,14 +74,18 @@ export default function SimpleCard(prop) {
                 <Typography className={classes.pos} color="textSecondary">
                     {prop.nameRead}
                 </Typography>
-                <Typography variant="body2" component="p">
-                    {prop.comment}
-                </Typography>
+                <Box className={classes.comment}>
+                    <Typography variant="body2" component="p">
+                        {prop.comment}
+                    </Typography>
+                </Box>
             </CardContent>
             <CardActions>
-                <Button variant="contained" className={classes.twitter} href={prop.link}>
-                    <FontAwesomeIcon icon={faTwitter} />
-                </Button>
+                <ThemeProvider theme={theme}>
+                    <IconButton href={prop.link} color="primary" rel="noopener noreferrer" target="_ blank" size="small" className={classes.button}>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </IconButton>
+                </ThemeProvider>
             </CardActions>
         </Card>
     );
