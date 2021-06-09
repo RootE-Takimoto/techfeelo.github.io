946698f0875434a3050921351c0db5671545605f
syobonpastel <syobon.pastel@gmail.com>
2021-06-09 22:31:47 +0900

    #8

---
+++ src/pages/about/public_notice.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <Seo title="決算公告" />
+    <h1>公告</h1>
+    <p>公告は以下からご覧ください。</p>
+  </Layout>
+)
+
+export default AboutPage
